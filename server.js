a7836dab411617429c6e897850d2c4756aba8afd
Glitch (hello-express) <none>
2019-03-07 02:57:02 +0000

    ✌️⏱ Checkpoint ./test.js:1868276/1683 ./server.js:1868276/465

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 
 // init project
 const express = require('express');
-const mongoose = require('mongoose');
-const mongo_url = "mongodb+srv://"+process.env.USERNAME+":"+process.env.PASSWORD+"@"+process.env.HOST+"/"+process.env.DATABASE;
-//console.log(mongo_url)
-mongoose.connect(mongo_url, {useNewUrlParser: true, retryWrites: true});
+// const mongoose = require('mongoose');
+// const mongo_url = "mongodb+srv://"+process.env.USERNAME+":"+process.env.PASSWORD+"@"+process.env.HOST+"/"+process.env.DATABASE;
+// //console.log(mongo_url)
+// mongoose.connect(mongo_url, {useNewUrlParser: true, retryWrites: true});
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://librarian:hZg5af1x6S8UuVcL@infsci2560-spring19-midgh.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  if (err){
+    console.log("Error");
+    console.log(err);
+  }
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 const app = express();
 
