fddcc637ead2afaabb93d7dc610026e75c103102
Glitch (hello-express) <none>
2019-11-12 05:06:02 +0000

    🕵👦 Checkpoint ./models/student.js:6342529/1079

--- models/student.js
+++ models/student.js
@@ -2,15 +2,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const studentSchema = new Schema(
-  {
-    studentID: {type: Number, required:true},
-    name: {type: String, required: true},
-    major: {type: String, required: true},
-    gpa: {type: Number, required: false},
-    classes: {type: [String], required:false}
-  }
-);
+const studentSchema = new Schema({
+  studentID: { type: Number, required: true },
+  name: { type: String, required: true },
+  major: { type: String, required: true },
+  gpa: { type: Number, required: false },
+  classes: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'class'}
+});
 
 // Export model
 module.exports = mongoose.model("student", studentSchema);
