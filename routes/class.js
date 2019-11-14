45422370cfc912da82d6349f9d8730d2691fa84b
Glitch (hello-express) <none>
2019-11-14 00:26:40 +0000

    🍛👨‍❤️‍👨 Checkpoint ./routes/class.js:6342529/5747 ./models/class.js:6342529/20 ./models/student.js:6342529/30

--- routes/class.js
+++ routes/class.js
@@ -1,36 +1,55 @@
 // Route handlers
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //import data models
 const Class = require("../models/class");
 
-// RETRIEVE all classes
-router.get("/", function(req,res){
-  Class.find({}, function (err, classes_list){
-    res.status(200).render("classes", {classes:classes_list});
-  });
+// RETRIEVE all classes with a optional query parameter
+router.get("/", function(req, res, next) {
+  // if users did not enter a query parameter
+  if (!req.query.classID) {
+    Class.find({})
+      //print out class names rather than id
+      .exec(function(err, classes) {
+        if (err) {
+          return next(err);
+        }
+        res.status(200).render("classes", {
+          classes: classes
+        });
+      });
+  }
+  //if users did
+  else {
+    Class.find({ major: req.query.c })
+      //print out class names rather than id
+      .populate("classes")
+      .exec(function(err, students) {
+        if (err) {
+          return next(err);
+        }
+        res.render("student", {
+          student: students
+        });
+      });
+  }
 });
-
-// RETRIEVE one specific class by classID
-router.get("/:classID", function(req,res){
-  Class.find({}, function (err, classes_list){
-    res.status(200).render("classes", {classes:classes_list});
-  });
-});
-
-module.exports = router;
 
 //create a class
 router.post("/", function(req, res) {
   var oneClass = new Class(req.body);
   oneClass.save(function(err) {
     if (err) {
-      res.status(400).render("error",
-        send(err);
+      res.status(401).render("error", {
+        status: 401,
+        message: err
+      });
     }
     res.status(201).render("class", {
-      oneClass:oneClass
+      oneClass: oneClass
     });
   });
 });
+
+module.exports = router;
