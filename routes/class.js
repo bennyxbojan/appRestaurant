f5bb22a74f510730ed23132050c3d1d59fb1fd6e
Glitch (hello-express) <none>
2019-11-14 00:15:38 +0000

    🎫👌 Checkpoint ./routes/class.js:6342529/335 ./views/class.ejs:6342529/113

--- routes/class.js
+++ routes/class.js
@@ -8,8 +8,29 @@ const Class = require("../models/class");
 // RETRIEVE all classes
 router.get("/", function(req,res){
   Class.find({}, function (err, classes_list){
-    res.render("classes", {classes:classes_list});
+    res.status(200).render("classes", {classes:classes_list});
+  });
+});
+
+// RETRIEVE one specific class by classID
+router.get("/:classID", function(req,res){
+  Class.find({}, function (err, classes_list){
+    res.status(200).render("classes", {classes:classes_list});
   });
 });
 
 module.exports = router;
+
+//create a class
+router.post("/", function(req, res) {
+  var oneClass = new Class(req.body);
+  oneClass.save(function(err) {
+    if (err) {
+      res.status(400).render("error",
+        send(err);
+    }
+    res.status(201).render("class", {
+      oneClass:oneClass
+    });
+  });
+});
