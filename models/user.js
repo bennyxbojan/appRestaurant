89ed1a22794aa523907550762c8f7566e3187439
Glitch (hello-express) <none>
2019-11-26 08:35:44 +0000

    🏏😂 Checkpoint ./models/user.js:6342529/71 ./routes/search.js:6342529/18

--- models/user.js
+++ models/user.js
@@ -54,8 +54,5 @@ UserSchema.pre('save', function (next) {
   })
 });
 
-
-var User = mongoose.model('User', UserSchema);
-module.exports = User;
 module.exports = mongoose.model('User', UserSchema);
 
