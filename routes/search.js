2ef0dd11acf374875f15c80bc23b996244e8bcf7
Glitch (hello-express) <none>
2019-11-26 08:02:39 +0000

    🛫🌑 Checkpoint ./routes/search.js:6342529/5686

--- routes/search.js
+++ routes/search.js
@@ -1,89 +1,103 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var User = require('../models/user');
-
+var User = require("../models/user");
 
 // GET route for reading data
-router.get('/', function (req, res, next) {
-  res.status(200).render("../public/login.html");
+router.get("/", function(req, res, next) {
+  if (req.body.date && req.body.time && req.body.guest && req.body.city) {
+    var query = {
+      date: req.body.date,
+      time: req.body.time,
+      guest: req.body.guest,
+      city: req.body.city
+    };
+  }
+  
 });
 
-
 //POST route for updating data
-router.post('/', function (req, res, next) {
+router.post("/", function(req, res, next) {
   // confirm that user typed same password twice
   if (req.body.password !== req.body.passwordRe) {
-    var err = new Error('Passwords do not match.');
+    var err = new Error("Passwords do not match.");
     err.status = 400;
     res.send("passwords dont match");
     return next(err);
   }
 
-  if (req.body.email &&
+  if (
+    req.body.email &&
     req.body.username &&
     req.body.password &&
-    req.body.passwordRe) {
-
+    req.body.passwordRe
+  ) {
     var userData = {
       email: req.body.email,
       username: req.body.username,
-      password: req.body.password,
-    }
+      password: req.body.password
+    };
 
-    User.create(userData, function (error, user) {
+    User.create(userData, function(error, user) {
       if (error) {
         return next(error);
       } else {
         req.session.userId = user._id;
-        return res.redirect('/profile');
+        return res.redirect("/profile");
       }
     });
-
   } else if (req.body.logusername && req.body.logpassword) {
-    User.authenticate(req.body.logusername, req.body.logpassword, function (error, user) {
+    User.authenticate(req.body.logusername, req.body.logpassword, function(
+      error,
+      user
+    ) {
       if (error || !user) {
-        var err = new Error('Wrong username or password.');
+        var err = new Error("Wrong username or password.");
         err.status = 401;
         return next(err);
       } else {
         req.session.userId = user._id;
-        return res.redirect('/login/profile');
+        return res.redirect("/login/profile");
       }
     });
   } else {
-    var err = new Error('All fields required.');
+    var err = new Error("All fields required.");
     err.status = 400;
     return next(err);
   }
-})
+});
 
 // GET route after registering
-router.get('/profile', function (req, res, next) {
-  User.findById(req.session.userId)
-    .exec(function (error, user) {
-      if (error) {
-        return next(error);
+router.get("/profile", function(req, res, next) {
+  User.findById(req.session.userId).exec(function(error, user) {
+    if (error) {
+      return next(error);
+    } else {
+      if (user === null) {
+        var err = new Error("Not authorized! Go back!");
+        err.status = 400;
+        return next(err);
       } else {
-        if (user === null) {
-          var err = new Error('Not authorized! Go back!');
-          err.status = 400;
-          return next(err);
-        } else {
-          return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/login/logout">Logout</a>')
-        }
+        return res.send(
+          "<h1>Name: </h1>" +
+            user.username +
+            "<h2>Mail: </h2>" +
+            user.email +
+            '<br><a type="button" href="/login/logout">Logout</a>'
+        );
       }
-    });
+    }
+  });
 });
 
 // GET for logout
-router.get('/logout', function (req, res, next) {
+router.get("/logout", function(req, res, next) {
   if (req.session) {
     // delete session object
-    req.session.destroy(function (err) {
+    req.session.destroy(function(err) {
       if (err) {
         return next(err);
       } else {
-        return res.redirect('/');
+        return res.redirect("/");
       }
     });
   }
