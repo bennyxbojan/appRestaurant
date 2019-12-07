09228df1f62805a608b92821d191004ec18abefd
Glitch (final-pro-yay76-feq) <none>
2019-12-07 22:39:40 +0000

    📕⛈ Checkpoint ./views/index.ejs:6342529/405 ./routes/index.js:6342466/2:6342529/929 ./routes/login.js:6342466/2

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,18 @@ const router = express.Router();
 // const Class = require("../models/class");
 
 //default link redirect to index html page
+
+var today = new Date();
+var dd = ("0" + today.getDate()).slice(-2);
+var mm = ("0" + (today.getMonth() + 1)).slice(-2);
+var yyyy = today.getFullYear();
+today = yyyy + "-" + mm + "-" + dd;
+
 router.get("/", function(req, res) {
   res.status(200);
-  res.render("index");
+  res.render("index",{
+    date:today  
+  })
 });
 
 //get all database record
