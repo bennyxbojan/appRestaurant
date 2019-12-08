70d73ff1ca6035d88b1f59d6238b45585818d2b5
Glitch (final-pro-yay76-feq) <none>
2019-12-08 00:11:38 +0000

    🍂🗻 Checkpoint ./views/login.ejs:6342466/290 ./routes/login.js:6342466/424 ./routes/index.js:6342466/2 ./views/profile.ejs:6342466/4

--- routes/login.js
+++ routes/login.js
@@ -116,14 +116,10 @@ router.get("/admin", checkAdmin, function(req, res, next) {
         err.status = 400;
         return next(err);
       } else {
-        return res.send(
-          "<h1>Admin Page</h1>" +
-            "<h1>Name: </h1>" +
-            user.username +
-            "<h2>Mail: </h2>" +
-            user.email +
-            '<br><a type="button" href="/logout">Logout</a>'
-        );
+        return res.render("profile", {
+          name: user.username,
+          email: user.email
+        });
       }
     }
   });
