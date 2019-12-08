4391f0ed848a9fc738f36740f4dc19a8a9b1a378
Glitch (final-pro-yay76-feq) <none>
2019-12-08 09:37:43 +0000

    🎣🏐 Checkpoint ./models/order.js:6342529/2462 ./routes/order.js:6342529/8

--- models/order.js
+++ models/order.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var orderSchema = new Schema({
-  _id:{
-    type:Number,
-    unique:true,
-    required:true,
-    
+  orderNum: {
+    type: Number,
+    unique: true,
+    required: true,
+    default: 10000
   },
   userID: {
     type: mongoose.Schema.Types.ObjectId,
@@ -25,14 +25,28 @@ var orderSchema = new Schema({
   },
   guest: {
     type: Number,
-    required:true,
+    required: true
   },
   tables: {
     type: mongoose.Schema.Types.ObjectId,
-    required:true,
+    required: true,
     ref: "Table"
   }
 });
 
+var Order = mongoose.model("Order", orderSchema);
+
+orderSchema.pre("save", function(next) {
+  var doc = this;
+  Order.findByIdAndUpdate(
+    { _id: "entityId" },
+    { $inc: { orderNum: 1 } },
+    function(error, order) {
+      if (error) return next(error);
+      doc.testvalue = order.orderNum;
+      next();
+    }
+  );
+});
 // Export model
 module.exports = mongoose.model("Order", orderSchema);
