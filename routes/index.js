f27134bc6592f98e717323db55496901647632f9
Glitch (final-pro-yay76-feq) <none>
2019-12-09 09:52:36 +0000

    🎆🐺 Checkpoint ./routes/index.js:6342529/485

--- routes/index.js
+++ routes/index.js
@@ -16,13 +16,24 @@ var NodeGeocoder = require('node-geocoder');
 
 // console.log(geo.city);
 
+var options = {
+  provider: 'google',
+ 
+  // Optional depending on the providers
+  httpAdapter: 'https', // Default
+  apiKey: 'YOUR_API_KEY', // for Mapquest, OpenCage, Google Premier
+  formatter: null         // 'gpx', 'string', ...
+};
+ 
+var geocoder = NodeGeocoder(options);
+
 var today = new Date();
 var dd = ("0" + today.getDate()).slice(-2);
 var mm = ("0" + (today.getMonth() + 1)).slice(-2);
 var yyyy = today.getFullYear();
 today = yyyy + "-" + mm + "-" + dd;
 
-router.get("/", function(req, res) {
+router.get("/", function(req, res, next) {
   //get client ip
   var ip =
     req.headers["x-forwarded-for"] ||
@@ -37,10 +48,16 @@ router.get("/", function(req, res) {
   var geo = geoip.lookup(client);
   console.log(geo);
   
-  //find the most popular rests
+  //find the closet rests
   var curr_ll = geo.ll;
   
-  
+  Restaurant.find({city: geo.city}, function(err,rests){
+    if (err){
+      return next(err);
+    }else{
+      
+    }
+  })
   
   res.render("index", {
     city: geo.city,
