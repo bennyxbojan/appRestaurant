97ad296759b85c8aaefb39af1539ee269668404e
Glitch (final-pro-yay76-feq) <none>
2019-12-09 08:54:35 +0000

    🎄🔦 Checkpoint ./public/index.css:6342466/423 ./views/index.ejs:6342466/3 ./views/review.ejs:6342466/28 ./views/login.ejs:6342466/2 ./routes/index.js:6342529/3707

--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,30 @@ const router = express.Router();
 // const Student = require("../models/student");
 // const Class = require("../models/class");
 //default link redirect to index html page
- 
 
-var city;
-var error;
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    error = "Geolocation is not supported by this browser.";
-  }
+function locationSuccess(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  var altitude = position.coords.altitude;
+  var accuracy = position.coords.accuracy;
+  var altitudeAccuracy = position.coords.altitudeAccuracy;
+  var heading = position.coords.height;
+  var speed = position.coords.speed;
+  var timestamp = position.timestamp;
+
+  // work with this information however you'd like!
 }
 
-function getCity(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
+function locationError(error) {
+  var code = error.code;
+  var message = error.message;
+
+  // read the code and message and decide how you want to handle this!
 }
 
-var today = new Date(); 
+navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
+
+var today = new Date();
 var dd = ("0" + today.getDate()).slice(-2);
 var mm = ("0" + (today.getMonth() + 1)).slice(-2);
 var yyyy = today.getFullYear();
@@ -31,11 +37,9 @@ today = yyyy + "-" + mm + "-" + dd;
 
 router.get("/", function(req, res) {
   res.status(200);
-  res.render("index",{
-    date:today  
-  })
-}); 
-
-
+  res.render("index", {
+    date: today
+  });
+});
 
 module.exports = router;
