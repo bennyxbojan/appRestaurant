4a2c56677244fe3dbfc22fdd35a2f47932784f48
Glitch (final-pro-yay76-feq) <none>
2019-12-09 20:54:39 +0000

    🎄🐮 Checkpoint ./views/restaurants.ejs:6342529/520 ./routes/search.js:6342529/5046

--- routes/search.js
+++ routes/search.js
@@ -11,7 +11,7 @@ const week = [
   "Thursday",
   "Friday",
   "Saturday"
-];  
+];
 
 function getTableSize(guest) {
   var size;
@@ -29,65 +29,77 @@ function getTableSize(guest) {
 
 // function validtime (time){
 //   var digit = time.substring(0,time.length-2);
-  
+
 //   var apm = time.substring(time.length-2,time.length);
 
 // }
 
 // console.log(validtime("1PM"));
 
-
 // GET route for reading data
 router.get("/", function(req, res, next) {
-  if (req.query.date && req.query.time && req.query.guest && req.query.city) {
-    var date = new Date(req.query.date);
-    var weekday = date.getDay();
-    var size = getTableSize(req.query.guest);
-    console.log(size);
+  //index page when users don't need to give restaurant name
+  if (!req.query.restname) {
+    if (req.query.date && req.query.time && req.query.guest && req.query.city) {
+      var date = new Date(req.query.date);
+      var weekday = date.getDay();
+      var size = getTableSize(req.query.guest);
+      console.log(size);
 
-    Table.find({ time: req.query.time, size: size }, function(err, table) {
-      if (err) {
-        return next(err);
-      } else {
-        var tableid = table[0]._id;
-         console.log(table[0]._id);
-      
-      // var option = {
-      //   table: tableid,
-      //   taken: false
-      // };
-      // console.log(option);
-
-      var result = Restaurant.find({
-        opendays: week[weekday],
-        city: req.query.city,
-        //options: option
-         options: { $elemMatch: { table: tableid, taken: false } },
-         // options: { $elemMatch: { taken: false } }
-      });
-      result.sort('name').populate("options.table").exec(function(err, rest) {
+      Table.find({ time: req.query.time, size: size }, function(err, table) {
         if (err) {
           return next(err);
         } else {
-          // console.log(rest)
-          console.log(tableid);
-          res.render("restaurants", {
-            rest: rest,
-            tableid:tableid,
-            date: req.query.date,
-            time: req.query.time,
-            guest: req.query.guest, 
-            city: req.query.city
+          var tableid = table[0]._id;
+          console.log(table[0]._id);
+
+          // var option = {
+          //   table: tableid,
+          //   taken: false
+          // };
+          // console.log(option);
+
+          var result = Restaurant.find({
+            opendays: week[weekday],
+            city: req.query.city,
+            //options: option
+            options: { $elemMatch: { table: tableid, taken: false } }
+            // options: { $elemMatch: { taken: false } }
           });
+          result
+            .sort("name")
+            .populate("options.table")
+            .exec(function(err, rest) {
+              if (err) {
+                return next(err);
+              } else {
+                // console.log(rest)
+                console.log(tableid);
+                res.render("restaurants", {
+                  rest: rest,
+                  tableid: tableid,
+                  date: req.query.date,
+                  time: req.query.time,
+                  guest: req.query.guest,
+                  city: req.query.city
+                });
+              }
+            });
         }
       });
-      };
-    });
+    } else {
+      var err = new Error("All fields required.");
+      err.status = 400;
+      return next(err);
+    }
+    //where users give restaurant name for searching
   } else {
-    var err = new Error("All fields required.");
-    err.status = 400;
-    return next(err);
+    
+    
   }
+  
+    
+  
 });
 
 module.exports = router;
